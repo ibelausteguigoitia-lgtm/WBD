7d2dca4baab7c488ef3a8d3305e38a1576168e12
ibelausteguigoitia-lgtm <ibelausteguigoitia@gmail.com>
2026-09-11 10:22:43 -0600

    Update SQLite.sql

--- SQLite.sql
+++ SQLite.sql
@@ -1,9 +1,10 @@
 ---Q1
-SELECT * , count(customer_id) as duplicate fROM sub
-group by customer_id
-having subscription_typ like '%paid%' and status like '%active%' and duplicate > 1
+SELECT customer_id, COUNT(DISTINCT subscription_id) AS duplicate_subscriptions FROM sub
+WHERE subscription_typ LIKE '%Paid%' AND status LIKE '%Active%'
+GROUP BY customer_id
+HAVING COUNT(DISTINCT subscription_id) > 1
 ---THOUGHT PROCESS:
----Identify duplicate customers, User must have paid and it's an active account.
+---Identify customers with more than one active paid subscription.
 
 ---Q2
 SELECT *, COUNT(payment_id) AS total_charges, MAX(REPLACE(billing_amount, '$', '') * 1.0) * (COUNT(payment_id) - 1) AS overbilled_amount FROM Pay
@@ -18,16 +19,18 @@ left join Pay
 on sub.customer_id=pay.customer_id and sub.renewal_date = pay.billing_cycle_da
 where sub.subscription_typ like '%paid%' and sub.status like '%Active%' and sub.expected_payment like '%Yes%' AND pay.payment_id IS NULL
 ---THOUGHT PROCESS
----Account must be active, subscription appeards as "Paid" (even if it's not the case", but the system is waiting for a payment to be done, that is to say Expected Payment = Yes.
+---Identify active paid subscriptions where a payment is expected but no matching payment exists for the renewal date.
 
 ---Q4 
-select * from sub
-where subscription_typ like '%Trial%' and status like '%Active%' and trial_end_date is not null
+SELECT sub.* FROM sub
+LEFT JOIN Pay
+ON sub.subscription_id = Pay.subscription_id
+WHERE sub.subscription_typ LIKE '%Trial%' AND sub.status LIKE '%Active%' AND sub.trial_end_date IS NOT NULL AND Pay.payment_id IS NULL
 ---THOUGHT PROCESS
----Identify all Trial Accounts + active accounts + accounts with a trial end date
+---Identify all Trial Accounts + active accounts + accounts with a trial end date + No payments an be done
 
 ---Q5
 Select *, ROUND((REPLACE(billing_amount, '$', '') * 1.0) - (REPLACE(psp_amount, '$', '') * 1.0), 2) as difference from pay
-where difference > 0
+WHERE ROUND( (REPLACE(billing_amount, '$', '') * 1.0) - (REPLACE(psp_amount, '$', '') * 1.0), 2) <> 0
 ---THOUGHT PROCESS
 ---Identify a difference between what we charged vs the money we got.
